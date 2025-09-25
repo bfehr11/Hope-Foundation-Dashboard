4d5f3952d11c976ca888bd5ea59890d15fe94194
bfehr11 <89813070+bfehr11@users.noreply.github.com>
2025-09-25 17:56:59 +0000

    mvp first page done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# read in data
+data = pd.read_excel("UNO Service Learning Data Sheet De-Identified Version.xlsx")
+
+# filter the data for only rows where there is no Reason and Request Status is "Pending"
+page1_data = data[data['Reason - Pending/No'].isna()]
+page1_data = page1_data[page1_data['Request Status'] == 'Pending'][['Patient ID#', 'Grant Req Date', 'Application Signed?']] # Filter out columns so that only necessary information will be visible
+page1_data['Application Signed?'] = "No" # All of the remaining rows have NaN as their Application Signed? value so replace it NaN with something readable
+
+st.title("Applications Ready to be Reviewed")
+st.dataframe(page1_data)
